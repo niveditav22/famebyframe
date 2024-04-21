218e621702bd6b8152d65ab68061424ad29f6b3d
niveditav22 <82770576+niveditav22@users.noreply.github.com>
2024-04-21 13:14:57 +0100

    Update feedRoutes.js

--- routes/feedRoutes.js
+++ routes/feedRoutes.js
@@ -1,13 +1,9 @@
-// feedRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const feedController = require('../controllers/feedController');
 
-// GET /feed route
 router.get('/', feedController.renderFeed);
 
-// GET /upload route (to render the upload form)
 router.get('/upload', feedController.renderUploadForm);
 
 module.exports = router;

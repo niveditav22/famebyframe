1a5ad2d6453cfe32837e8ac70f7d0332742b868b
niveditav22 <82770576+niveditav22@users.noreply.github.com>
2024-04-21 13:13:07 +0100

    Update imagesController.js

--- controllers/imagesController.js
+++ controllers/imagesController.js
@@ -1,9 +1,5 @@
 const Image = require('../models/Image');
-//const resizeImage = require('../resize');
 const sharp = require('sharp');
-const fs = require('fs');
-const path = require('path');
-
 
 exports.uploadImage = async (req, res) => {
     console.log('Received upload request');
@@ -11,38 +7,38 @@ exports.uploadImage = async (req, res) => {
     try {
         if (!req.files || Object.keys(req.files).length === 0 || !req.files.image) {
             console.log('No files uploaded');
-            req.flash('error', 'No file was uploaded.'); // Add a flash message for no file uploaded
-            return res.status(400).redirect('/upload'); // Redirect back to the upload page
+            req.flash('error', 'No file was uploaded.'); 
+            return res.status(400).redirect('/upload'); 
         }
 
         console.log('Processing file:', req.files.image.name);
         const imageFile = req.files.image;
         const caption = req.body.caption;
         const altText = req.body.altText;
-        const filename = `resized-${imageFile.name}`; // Prefix filename to indicate it's resized
+        const filename = `resized-${imageFile.name}`; 
         const uploadPath = `./uploads/${imageFile.name}`;
         const resizedPath = `./uploads/${filename}`;
 
-        // Move the uploaded file to the uploads directory
+    
         await imageFile.mv(uploadPath);
 
-        // Resize the image to a width of 400 pixels while maintaining aspect ratio
+
         await sharp(uploadPath)
-            .resize(400, 400) // Setting height to null maintains aspect ratio
+            .resize(400, 400)
             .toFile(resizedPath);
 
         console.log('Saving to database');
-        // Assuming saveToDatabase expects: caption, altText, username, filename, userId
+        
         await Image.saveToDatabase(caption, altText, req.session.username, filename, req.session.userId);
 
         console.log('Redirecting to user page');
-        req.flash('success', 'Image uploaded and resized successfully!'); // Add a flash message for successful upload
-        res.redirect('/users/userPage'); // Redirect to the user's page
+        req.flash('success', 'Image uploadeds uccessfully!'); 
+        res.redirect('/users/userPage'); 
 
     } catch (error) {
         console.error('Error handling file upload:', error);
-        req.flash('error', 'Error handling file upload.'); // Add a flash message for the error
-        res.status(500).redirect('/upload'); // Redirect back to the upload page
+        req.flash('error', 'Error handling file upload.'); 
+        res.status(500).redirect('/upload'); 
     }
 };
 

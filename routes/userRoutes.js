e160ba6c6ec36d2a618f295850c1829dda9b04cd
niveditav22 <82770576+niveditav22@users.noreply.github.com>
2024-04-21 13:15:30 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,25 +1,11 @@
-// userRoutes.js
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const ImageController = require('../controllers/imagesController');
-const upload = require('../multerConfig');
 
-// Registration
 router.post('/register', userController.registerUser);
 
-// Login
 router.post('/login', userController.loginUser);
 
-
-// Display user page
 router.get('/userPage', userController.getUserPage);
-router.get('/userPage', userController.getLoggedInUserProfile);
-router.get('/:username', userController.getUserProfile);
-
-
-//router.post('/images/upload', upload.single('image'), ImageController.uploadImage);
-
-
 
 module.exports = router;

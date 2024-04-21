7949888cadae91d399a09208171b130c8fc4e7e6
niveditav22 <82770576+niveditav22@users.noreply.github.com>
2024-04-21 13:16:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,18 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const pool = require("./database"); 
-const bodyParser = require('body-parser');
 const app = express();
 const flash = require('express-flash');
 const morgan = require('morgan');
 const fileUpload = require('express-fileupload');
 
-
+//routes 
 const userRoutes = require('./routes/userRoutes');
 const imagesRoutes = require('./routes/imagesRoutes');
 const feedRoutes = require('./routes/feedRoutes');
 
 
-// middleware
+//middleware
 app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
@@ -22,7 +21,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
-// session
+//sessions
 app.use(session({
     secret: 'walking-scott-street-feeling-like-a-stranger',
     resave: false,
@@ -41,18 +40,17 @@ app.use('/public', express.static('public', {
     }
 }));
 
-// Set up view engine
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// File upload middleware
 app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: '/tmp/',
     limits: { fileSize: 10 * 1024 * 1024 } // 10MB
 }));
 
-// Routes
+
 app.use('/users', userRoutes);
 app.use('/upload', imagesRoutes);
 app.use('/feed', feedRoutes);
@@ -64,31 +62,27 @@ app.use((req, res, next) => {
 app.use('/uploads/resized', express.static(path.join(__dirname, 'assets', 'uploads', 'resized')));
 
 
-// Define login and register routes
+
 app.get('/login', (req, res) => {
-    res.render('login'); // render login.ejs
+    res.render('login'); 
 });
 
 app.get('/register', (req, res) => {
-    res.render('register'); // render register.ejs
+    res.render('register'); 
 });
 
 app.get('/upload', (req, res) => {
     res.render('upload');
 });
 
-app.get('/users/:username', (req, res) => {
-    const username = req.params.username;
-});
 
-// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
 });
 
-// Start the server
-const PORT = process.env.PORT || 2000;
+
+const PORT = process.env.PORT || 2002;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
